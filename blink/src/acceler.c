836230b0e8b4259f065172667d49c8a7932c90ed
Jippe Holwerda <j.holwerda@voormedia.com>
2013-03-02 13:17:30 +0100

    Almost working Makefile.

--- blink/src/acceler.c
+++ blink/src/acceler.c
@@ -16,10 +16,12 @@ int main(void)
 }
 
 void setup() {
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
 }
 
 void loop() {
+  Serial.println(1);
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
