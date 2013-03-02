701e8702c1e8dd537eb448a0666f90550bf13b5b
Jippe Holwerda <j.holwerda@voormedia.com>
2013-03-02 12:04:19 +0100

    Added missing files.

--- blink/src/acceler.c
+++ blink/src/acceler.c
@@ -6,15 +6,10 @@ int main(void)
 {
   init();
 
-#if defined(USBCON)
-  USBDevice.attach();
-#endif
-
   setup();
 
   for (;;) {
     loop();
-    if (serialEventRun) serialEventRun();
   }
 
   return 0;
